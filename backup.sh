e8aeef77baf6f5fc4c3627be685c805b8042c022
wafsek <saraibaljit@gmail.com>
2020-01-29 21:39:59 +0100

    Added info comment to the script

--- backup.sh
+++ backup.sh
@@ -1,3 +1,13 @@
+#!/bin/bash
+<<COMMENT
+  A few things to note when using this script.
+  Do not put the script into any of the directories.
+  This script presumes that the backup folder and
+  working folder are right next to each other and that
+  the script it self is at the same dir level as them.
+  Change that before using the script. :)
+COMMENT
+
 backup_dir="backup"
 working_dir="working"
 temp_dir="temp"
